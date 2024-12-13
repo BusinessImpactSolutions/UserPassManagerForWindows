b4b8bf0a92847b32399e37a17ad211f3de337184
Bizz Land <github@bislinks.com>
2024-12-13 09:27:22 -0600

    Added/Tested dist.ini: OK
    Added dist.ini and tested: OK

---
+++ lib/UserPassManagerForWindows.pm
@@ -0,0 +1,22 @@
+package UserPassManagerForWindows;
+
+# ABSTRACT: User Pass Manager For Windows
+
+=head1 NAME
+
+UserPassManagerForWindows
+
+=head2 DESCRIPTION
+
+User Pass Manager For Windows
+
+Commandline Utility
+
+=head2 Usage
+
+See README.md
+
+=cut
+
+1;
+
